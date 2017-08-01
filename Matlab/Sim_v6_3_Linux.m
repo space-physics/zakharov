b8c63220cdfa5e1d10c3891adfebac48f7250b9b
scivision <scivision@users.noreply.github.com>
2017-08-01 02:43:44 -0400

    unify output

--- Matlab/Sim_v6_3_Linux.m
+++ Matlab/Sim_v6_3_Linux.m
@@ -19,7 +19,7 @@ if nargin < 1
 end
 
 if nargin < 2 || isempty(newpath)
-newpath='hc/test16';    useSinglePrec = false;
+newpath='/tmp/test';    useSinglePrec = false;
 end
 
 if nargin < 3
@@ -29,56 +29,25 @@ end
 noCaxis = true;
 
 
-for Nnbeam=0:0
-for Nvbeam=0:0
+for Nnbeam=1:1
+for Nvbeam=1:1
     close all;
-    instantiation = [newpath,': n',int2str(Nnbeam),'_v',int2str(Nvbeam)];
+    instantiation = [newpath,': n',num2str(Nnbeam,'%03d'),'_v',num2str(Nvbeam,'%03d')];
     
     if doWriteResults
-     mkdir([newpath,filesep, 'results'], ['n' num2str(Nnbeam) '_v' num2str(Nvbeam)]);
+     mkdir([newpath,filesep, 'results'], ['n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d')]);
     end
-%fclose('all');
 
-fnParam = [newpath,filesep,'parameters_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
-params = Langmuir_read_params(fnParam);
 
-fnOutput = [newpath,filesep,'output1_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
+fnParam = [newpath,filesep,'parameters_n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d') '.bin'];
+P = Langmuir_read_params(fnParam);
+N = P.N;
+
+fnOutput = [newpath,filesep,'output1_n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d') '.bin'];
 output1=Langmuir_read_output(fnOutput);
 
-pi=params(1);
-me=params(2);
-electroncharge=params(3);
-mi=params(4);
-Kb=params(5);
-eV=params(6);
-epsilon0=params(7);
-Z=params(8);
-Te=params(9);
-Ti=params(10);
-nuic=params(11);
-nuec=params(12);
-n0=params(13);
-nbeam=params(14);
-vbeam_ev=params(15);
-vbeam=params(16);
-tetabeam=params(17);
-endTime=params(18);
-Tstep=params(19);
-TT=params(20);
-res=params(21);
-TT_res=params(22);
-L=params(23);
-N=params(24);
-Xstep=params(25);
-QW=params(26);
-SEED=params(27);
-eta=params(28);
-ve=params(29);
-Cs=params(30);
-omegae=params(31);
-lambdaD=params(32);
-j=1j;
-omega_off=omegae+2*pi*300000;
+
+omega_off=P.omegae+2*pi*300000;
 
 p=zeros(1,N);
 k=zeros(1,N);
@@ -120,15 +89,15 @@ end
 %kk_ss=[756 876 1100];
 kk_ss=(1:length(k));
 [aa, bb]=size(kk_ss);
-EE_ss=zeros(bb,TT_res);
-nn_ss=zeros(bb,TT_res);
+EE_ss=zeros(bb,P.TT_res);
+nn_ss=zeros(bb,P.TT_res);
 
-taxis=(1:TT_res)*Tstep*res*1000;
-for ii=1:QW
+taxis=(1:P.TT_res)*P.Tstep*P.res*1000;
+for ii=1:P.QW
 
 
-fnNN=[newpath,filesep,'nn' num2str(SEED+ii-1) '_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
-fnEE=[newpath,filesep,'EE' num2str(SEED+ii-1) '_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
+fnNN=[newpath,filesep,'nn' num2str(P.SEED,'%03d'), num2str(ii,'%03d'), '_n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d') '.bin'];
+fnEE=[newpath,filesep,'EE' num2str(P.SEED,'%03d'), num2str(ii,'%03d'), '_n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d') '.bin'];
 
 [nnComplex,EEComplex] = Langmuir_read_data(fnNN,fnEE,useSinglePrec);
 nn2=abs(nnComplex).^2;
@@ -149,7 +118,7 @@ display('entering for jj=1:bb')
             %}
         %end
 
-        ttt = 1:TT_res;
+        ttt = 1:P.TT_res;
         baseInd = (ttt-1).*N + kk_ss(jj);
         
         EE_ss(jj,:)=EE_ss(jj,:) +EE2(baseInd); %+buffer1(1,:);    
@@ -167,8 +136,8 @@ display('entering for jj=1:bb')
 %floor(ii/QW*100)
 
 end
-EE_ss=EE_ss/QW;
-nn_ss=nn_ss/QW;
+EE_ss=EE_ss/P.QW;
+nn_ss=nn_ss/P.QW;
 
  
 
@@ -194,7 +163,7 @@ ylabel('Wave number');
 title([instantiation,'  |E|^2 (dB)'])
 colorbar;
 if doWriteResults
- saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam) '_v' num2str(Nvbeam),filesep, 'E_kt'],'fig');
+ saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d'),filesep, 'E_kt'],'fig');
 end
 figure;
 imagesc(taxis,k,10*log10(abs(nn_ss)))
@@ -203,21 +172,21 @@ ylabel('Wave number');
 title([instantiation,'|n|^2 (dB)'])
 colorbar;
 if doWriteResults
- saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam) '_v' num2str(Nvbeam),filesep,'n_kt'],'fig');
+ saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d'),filesep,'n_kt'],'fig');
 end
 
 figure(100);
-X1=linspace(0,1/3*sqrt(me/mi)/lambdaD,100);
-Y1=(X1*lambdaD).^2;
+X1=linspace(0,1/3*sqrt(P.me/P.mi)/P.lambdaD,100);
+Y1=(X1*P.lambdaD).^2;
 % figure(101);
 semilogy(X1,Y1,'k','LineWidth',3);
 hold on;
 % semilogy(-X1,Y1,'k','LineWidth',3);
 X2=X1(end)+linspace(0,k(end),1000);
-Y2=X2*lambdaD*sqrt(me/mi); Y2=Y2-Y2(1)+Y1(end);
+Y2=X2*P.lambdaD*sqrt(P.me/P.mi); Y2=Y2-Y2(1)+Y1(end);
 semilogy(X2,Y2,'k','LineWidth',3);
 % semilogy(-X2,Y2,'k','LineWidth',3);
-Y2=(X2*lambdaD).^4*mi/me; Y2=Y2-Y2(1)+Y1(end);
+Y2=(X2*P.lambdaD).^4*P.mi/P.me; Y2=Y2-Y2(1)+Y1(end);
 semilogy(X2,Y2,'k','LineWidth',3);
 % semilogy(-X2,Y2,'k','LineWidth',3);
 Y2=Y1(end)*ones(1,length(X1));
@@ -233,16 +202,16 @@ title([instantiation])
 resolution=1.0; %in ms
 resolution=length(taxis)/(taxis(end)/resolution);
 [EE_max, kk_max]=max(EE_ss);
-W=.5*epsilon0*(EE_max(resolution:resolution:end))/(n0*Kb*Te);
+W=.5*P.epsilon0*(EE_max(resolution:resolution:end))/(P.n0*P.Kb*P.Te);
 figure(100);
 hold on;
-semilogy(abs(k(kk_max(resolution:resolution:end))),(W));
-semilogy(abs(k(kk_max(resolution:resolution:end))),(W),'b*');
+semilogy(abs(k(kk_max(resolution:resolution:end))),W);
+semilogy(abs(k(kk_max(resolution:resolution:end))),W,'b*');
 hold off;
 xlabel('Wave number (m^-^1)');
 ylabel('Electrostatic to thermal energy ration');
 if doWriteResults
- saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam) '_v' num2str(Nvbeam),filesep,'regiems'],'fig');
+ saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d'),filesep,'regiems'],'fig');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -291,15 +260,15 @@ end
 
     Taverage=(.5e-3);
     OO=80; %how many periods?
-    ACF_length = floor(Taverage/(Tstep*res));
+    ACF_length = floor(Taverage/(P.Tstep*P.res));
 
     ACF_E=zeros(OO,N,2*ACF_length-1);
     ACF_n=zeros(OO,N,2*ACF_length-1);
     Spec_E=zeros(N,2*ACF_length-1);
     Spec_n=zeros(N,2*ACF_length-1);
-    taxis=(1:TT_res)*Tstep*res*1000;
+    taxis=(1:P.TT_res)*P.Tstep*P.res*1000;
     
-for ii=1:QW
+for ii=1:P.QW
 %   fnNN=[newpath,filesep,'nn' num2str(SEED+ii-1) '_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
 %   fnEE=[newpath,filesep,'EE' num2str(SEED+ii-1) '_n' num2str(Nnbeam) '_v' num2str(Nvbeam) '.bin'];
 %   [nnComplex,EEComplex] = Langmuir_read_data(fnNN,fnEE,useSinglePrec);
@@ -325,7 +294,7 @@ for kk=1:N
     %}
     ttt=TT_res-ACF_length:TT_res-1;
     %outInd = ttt-(TT_res-ACF_length)+1;
-    baseInd = (ttt-1).*N + kk;
+    baseInd = (ttt-1).* N + kk;
     
     ACF_E(oo,kk,:)=ACF_E(oo,kk,:) +xcorr(EEComplex(baseInd));%+xcorr(AA);
     %Spec_E(kk,:)=fftshift(fft(ACF_E(kk,:)))/(ACF_length*2-1);
@@ -339,10 +308,10 @@ end
 
 
 %floor(ii/QW*100)
-if ~mod(ii,1), display(['ii/QW: ',num2str(ii/QW*100,'%0.3f'),'% complete']),end
+if ~mod(ii,1), display(['ii/QW: ',num2str(ii/P.QW*100,'%0.3f'),'% complete']),end
 end
-ACF_E=ACF_E/QW/(ACF_length-1)*2;
-ACF_n=ACF_n/QW/(ACF_length-1)*2;
+ACF_E=ACF_E/P.QW/(ACF_length-1)*2;
+ACF_n=ACF_n/P.QW/(ACF_length-1)*2;
 
 
 
@@ -366,7 +335,7 @@ ACF_n=ACF_n/QW/(ACF_length-1)*2;
 % title([instantiation,'|n|^2 thermal level'])
 
 
-faxis=linspace(-1/(res*Tstep)/2,1/(res*Tstep)/2,ACF_length*2-1);
+faxis=linspace(-1/(P.res*P.Tstep)/2,1/(P.res*P.Tstep)/2,ACF_length*2-1);
 TT_res=length(taxis);
 % TT_res=TT_res-ACF_length*30;
 for oo=1:OO
@@ -390,7 +359,7 @@ title([instantiation,' "Spec_E"  Time: ', num2str(taxis(TT_res-ACF_length)), '--
 if noCaxis, else caxis([-80 -40]), end %#ok<UNRCH>
 if doWriteResults
  nameWr=['specE--' num2str(oo)];
- saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam) '_v' num2str(Nvbeam),filesep, nameWr],'fig');
+ saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d'),filesep, nameWr],'fig');
 end
 
 figure;
@@ -405,7 +374,7 @@ title([instantiation,' "Spec_n" Time: ', num2str(taxis(TT_res-ACF_length)), '--'
 if noCaxis, else caxis([80 120]), end %#ok<UNRCH>
 if doWriteResults
  nameWr=['specn--' num2str(oo)];
- saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam) '_v' num2str(Nvbeam),filesep, nameWr],'fig');
+ saveas(gcf,[newpath,filesep,'results',filesep,'n' num2str(Nnbeam,'%03d') '_v' num2str(Nvbeam,'%03d'),filesep, nameWr],'fig');
 end
 
 TT_res=TT_res-ACF_length;

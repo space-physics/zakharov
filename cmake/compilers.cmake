a13c4049b2111395977e6d2742dc3b6e98c0d92a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-11-27 23:27:17 -0500

    modern cmake

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -6,27 +6,31 @@ endif()
 
 set(CMAKE_CXX_STANDARD 17)
 
-if(${CMAKE_Fortran_COMPILER_ID} STREQUAL GNU)
-
-  find_package(OpenCoarrays)
-  if(OpenCoarrays_FOUND)
-    list(APPEND OpenCoarrays_LIBRARIES caf_mpi)
-    list(APPEND FFLAGS -fcoarray=lib)
-  else()
-    list(APPEND FFLAGS -fcoarray=single)
-  endif()
+if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
 
   add_compile_options(-mtune=native -Wall -Wextra -Wpedantic -fexceptions -Werror=array-bounds)
 
-  list(APPEND FFLAGS -Warray-temporaries -Wconversion -fimplicit-none 
--fcheck=all -ffpe-trap=invalid,zero,overflow)
+  list(APPEND FFLAGS -Warray-temporaries -Wconversion -fimplicit-none)
+  
+  if(CMAKE_BUILD_TYPE STREQUAL Debug)
+    list(APPEND FFLAGS -fcheck=all -ffpe-trap=invalid,zero,overflow)
+  endif()
+  
+  if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
+    list(APPEND FFLAGS -std=f2018)
+  endif()
 
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
 
-elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL Intel)
-  list(APPEND FFLAGS -warn -fpe0 -traceback -debug extended)# -check all -coarray )
-elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL PGI)
+  list(APPEND FFLAGS -warn -traceback -coarray)
+  
+  if(CMAKE_BUILD_TYPE STREQUAL Debug)
+    list(APPEND FFLAGS -fpe0 -debug extended -check all)
+  endif()
 
-elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL Flang)  
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
+
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Flang)  
   list(APPEND FFLAGS -Mallocatable=03)
   list(APPEND FLIBS -static-flang-libs)
 endif()
